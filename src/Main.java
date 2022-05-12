5319f27388bc83931b588128579a4c2514c1f36a
isabe <is2430803@gmail.com>
2022-05-11 22:26:19 -0300

    fist commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        double b,a,area1,area2;
+
+        b = 45;
+        a = 5;
+        area1 = b * a;
+        area2 = (b * a) / 2;
+
+        System.out.println("A area do retangulo: " + (a * b));
+        System.out.println(area1 + (b * a));
+        System.out.println("A area do triangulo: "+ (b * a) / 2);
+        System.out.println(area2 + (b * a) / 2);
+
+
+    }
+
+    }
+
+
+
+
